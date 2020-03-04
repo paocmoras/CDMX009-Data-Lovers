af4ce04787612760bb4a6a5bcc98c260e186003c
paocmoras <paocmoras@hotmail.com>
2020-03-04 12:38:15 -0600

    css,html,js,img

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { example } from './data.js';
+//import { example } from './data.js';
+$(".menu-bar").on("click", function(){
+  $(".contenido").toggleClass("abrir");
+  console.log("menu")
+});
 
-console.log(example);
+let searchbar = document.querySelector(".search");
 
+function slide() {
+  if (searchbar.classList.contains("hide")){
+      searchbar.classList.remove("hide");
+  }
+else{
+      searchbar.classList.add("hide");
+}
+  }
+
+
+//console.log(example);
