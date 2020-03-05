25b3fc3f3cde8b9bde4374236e2085bd9c8f2e73
paola <adripaogui@gmail.com>
2020-03-05 11:15:40 -0600

    obteniendo cambios

--- src/data.js
+++ src/data.js
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 // import data from './data/pokemon/pokemon.js';
 
 export const example = () => {
   return 'example';
 };
+=======
+import data from './data/pokemon/pokemon.js';
+export const pokemon =  data.pokemon;
+let date = lis.name;
+console.log(date);
+ 
+>>>>>>> cambios para hacer pull
--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
+<<<<<<< HEAD
 //import { example } from './data.js';
 
 //--------------------------------Menu------------------------------------------
 
+=======
+import pokemon  from './data.js';
+console.log(pokemon);
+>>>>>>> cambios para hacer pull
 $(".menu-bar").on("click", function(){
   $(".contenido").toggleClass("abrir");
   console.log("menu")
 });
 
+<<<<<<< HEAD
 
 
 //console.log(example);
+=======
+let searchbar = document.querySelector(".search");
+
+function slide() {
+  if (searchbar.classList.contains("hide")){
+      searchbar.classList.remove("hide");
+  }
+else{
+      searchbar.classList.add("hide");
+}
+  }
+ 
+>>>>>>> cambios para hacer pull
