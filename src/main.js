12fa59bfb110adbf7a7b17f5ac80489dfe1f5782
paocmoras <paocmoras@hotmail.com>
2020-03-05 10:24:22 -0600

    modificando el search, estilos de titulo

--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
 //import { example } from './data.js';
+
+//--------------------------------Menu------------------------------------------
+
 $(".menu-bar").on("click", function(){
   $(".contenido").toggleClass("abrir");
   console.log("menu")
 });
 
-let searchbar = document.querySelector(".search");
-
-function slide() {
-  if (searchbar.classList.contains("hide")){
-      searchbar.classList.remove("hide");
-  }
-else{
-      searchbar.classList.add("hide");
-}
-  }
 
 
 //console.log(example);
