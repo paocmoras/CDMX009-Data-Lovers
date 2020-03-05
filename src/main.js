f8d38b37172ad6490b9d4d3ef649668e91b5eac5
paola <adripaogui@gmail.com>
2020-03-05 12:53:52 -0600

    funciones

--- src/main.js
+++ src/main.js
@@ -1,31 +1,5 @@
-<<<<<<< HEAD
-//import { example } from './data.js';
-
-//--------------------------------Menu------------------------------------------
-
-=======
-import pokemon  from './data.js';
-console.log(pokemon);
->>>>>>> cambios para hacer pull
-$(".menu-bar").on("click", function(){
-  $(".contenido").toggleClass("abrir");
-  console.log("menu")
-});
-
-<<<<<<< HEAD
+import {pakiman}  from './data';
 
 
-//console.log(example);
-=======
-let searchbar = document.querySelector(".search");
 
-function slide() {
-  if (searchbar.classList.contains("hide")){
-      searchbar.classList.remove("hide");
-  }
-else{
-      searchbar.classList.add("hide");
-}
-  }
- 
->>>>>>> cambios para hacer pull
+console.log(pakiman())
