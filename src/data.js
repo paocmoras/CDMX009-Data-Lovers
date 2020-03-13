212c753269f51a6f0ff622539ac1a7cf98c712a2
paocmoras <paocmoras@hotmail.com>
2020-03-13 08:42:39 -0600

    todos los pokemones por tipo

--- src/data.js
+++ src/data.js
@@ -15,7 +15,7 @@ let list = data.pokemon;
 
 export const typeWater = () =>{
     //crea lista de los pokemones de tipo water
-       let agua = list.filter(c=>c.type.includes('Water'))
+       let agua = list.filter(w=>w.type.includes('Water'))
            agua.filter(w=>w.type.includes('Water'))
         //console.log(agua)
        let div = document.querySelector('#root')
@@ -33,23 +33,270 @@ export const typeWater = () =>{
            div.appendChild(d)
         });
 }
-    
+
 export const typeFire = () => {
     let fuego = list.filter(f=>f.type.includes('Fire'))
-     fuego.filter(fu=>fu.type.includes('Fire'))
+     fuego.filter(f=>f.type.includes('Fire'))
      let div =document.querySelector('#root')
      fuego.map(f=> {
         let d = document.createElement('d')
         d.classList.add("card")
           d.innerHTML = `
          <img src=${f.img} />
-          <p>${f.id}</p>
+         <p>${f.id}</p>
          <p>${f.name}</p>
           <p>${f.weaknesses}</p>
           <p>${f.type}</p>
           <p>${f.egg}</p>
          `
           div.appendChild(d)
-       })
-     
-    }
+       });
+
+}
+
+export const typeGrass = () =>{
+       let planta = list.filter(gr=>gr.type.includes('Grass'))
+           planta.filter(gr=>gr.type.includes('Grass'))
+       let div = document.querySelector('#root')
+        planta.map(gr =>{
+         let d = document.createElement('d')
+         d.classList.add("card")
+           d.innerHTML = `
+          <img src=${gr.img} />
+          <p>${gr.id}</p>
+          <p>${gr.name}</p>
+           <p>${gr.weaknesses}</p>
+           <p>${gr.type}</p>
+           <p>${gr.egg}</p>
+          `
+           div.appendChild(d)
+        });
+}
+
+export const typePoison = () =>{
+       let veneno = list.filter(po=>po.type.includes('Poison'))
+           veneno.filter(po=>po.type.includes('Poison'))
+       let div = document.querySelector('#root')
+        veneno.map(po =>{
+         let d = document.createElement('d')
+         d.classList.add("card")
+           d.innerHTML = `
+          <img src=${po.img} />
+          <p>${po.id}</p>
+          <p>${po.name}</p>
+           <p>${po.weaknesses}</p>
+           <p>${po.type}</p>
+           <p>${po.egg}</p>
+          `
+           div.appendChild(d)
+        });
+}
+
+export const typeFlying = () =>{
+       let volador = list.filter(fl=>fl.type.includes('Flying'))
+           volador.filter(fl=>fl.type.includes('Flying'))
+       let div = document.querySelector('#root')
+        volador.map(fl =>{
+         let d = document.createElement('d')
+         d.classList.add("card")
+           d.innerHTML = `
+          <img src=${fl.img} />
+          <p>${fl.id}</p>
+          <p>${fl.name}</p>
+           <p>${fl.weaknesses}</p>
+           <p>${fl.type}</p>
+           <p>${fl.egg}</p>
+          `
+           div.appendChild(d)
+        });
+}
+
+export const typeBug = () =>{
+       let bicho = list.filter(b=>b.type.includes('Bug'))
+           bicho.filter(b=>b.type.includes('Bug'))
+       let div = document.querySelector('#root')
+        bicho.map(b =>{
+         let d = document.createElement('d')
+         d.classList.add("card")
+           d.innerHTML = `
+          <img src=${b.img} />
+          <p>${b.id}</p>
+          <p>${b.name}</p>
+           <p>${b.weaknesses}</p>
+           <p>${b.type}</p>
+           <p>${b.egg}</p>
+          `
+           div.appendChild(d)
+        });
+}
+
+export const typeNormal = () =>{
+       let normal = list.filter(n=>n.type.includes('Normal'))
+           normal.filter(n=>n.type.includes('Normal'))
+       let div = document.querySelector('#root')
+        normal.map(n =>{
+         let d = document.createElement('d')
+         d.classList.add("card")
+           d.innerHTML = `
+          <img src=${n.img} />
+          <p>${n.id}</p>
+          <p>${n.name}</p>
+           <p>${n.weaknesses}</p>
+           <p>${n.type}</p>
+           <p>${n.egg}</p>
+          `
+           div.appendChild(d)
+        });
+}
+
+export const typeElectric = () =>{
+       let electrico = list.filter(el=>el.type.includes('Electric'))
+           electrico.filter(el=>el.type.includes('Electric'))
+       let div = document.querySelector('#root')
+        electrico.map(el =>{
+         let d = document.createElement('d')
+         d.classList.add("card")
+           d.innerHTML = `
+          <img src=${el.img} />
+          <p>${el.id}</p>
+          <p>${el.name}</p>
+           <p>${el.weaknesses}</p>
+           <p>${el.type}</p>
+           <p>${el.egg}</p>
+          `
+           div.appendChild(d)
+        });
+}
+
+export const typeGround = () =>{
+       let tierra = list.filter(gro=>gro.type.includes('Ground'))
+           tierra.filter(gro=>gro.type.includes('Ground'))
+       let div = document.querySelector('#root')
+        tierra.map(gro =>{
+         let d = document.createElement('d')
+         d.classList.add("card")
+           d.innerHTML = `
+          <img src=${gro.img} />
+          <p>${gro.id}</p>
+          <p>${gro.name}</p>
+           <p>${gro.weaknesses}</p>
+           <p>${gro.type}</p>
+           <p>${gro.egg}</p>
+          `
+           div.appendChild(d)
+        });
+}
+
+export const typeFighting = () =>{
+       let lucha = list.filter(fig=>fig.type.includes('Fighting'))
+           lucha.filter(fig=>fig.type.includes('Fighting'))
+       let div = document.querySelector('#root')
+        lucha.map(fig =>{
+         let d = document.createElement('d')
+         d.classList.add("card")
+           d.innerHTML = `
+          <img src=${fig.img} />
+          <p>${fig.id}</p>
+          <p>${fig.name}</p>
+           <p>${fig.weaknesses}</p>
+           <p>${fig.type}</p>
+           <p>${fig.egg}</p>
+          `
+           div.appendChild(d)
+        });
+}
+
+export const typePsychic = () =>{
+       let psiquico = list.filter(ps=>ps.type.includes('Psychic'))
+           psiquico.filter(ps=>ps.type.includes('Psychic'))
+       let div = document.querySelector('#root')
+        psiquico.map(ps =>{
+         let d = document.createElement('d')
+         d.classList.add("card")
+           d.innerHTML = `
+          <img src=${ps.img} />
+          <p>${ps.id}</p>
+          <p>${ps.name}</p>
+           <p>${ps.weaknesses}</p>
+           <p>${ps.type}</p>
+           <p>${ps.egg}</p>
+          `
+           div.appendChild(d)
+        });
+}
+
+export const typeRock = () =>{
+       let roca = list.filter(r=>r.type.includes('Rock'))
+           roca.filter(r=>r.type.includes('Rock'))
+       let div = document.querySelector('#root')
+        roca.map(r =>{
+         let d = document.createElement('d')
+         d.classList.add("card")
+           d.innerHTML = `
+          <img src=${r.img} />
+          <p>${r.id}</p>
+          <p>${r.name}</p>
+           <p>${r.weaknesses}</p>
+           <p>${r.type}</p>
+           <p>${r.egg}</p>
+          `
+           div.appendChild(d)
+        });
+}
+
+export const typeIce = () =>{
+       let hielo = list.filter(i=>i.type.includes('Ice'))
+           hielo.filter(i=>i.type.includes('Ice'))
+       let div = document.querySelector('#root')
+        hielo.map(i =>{
+         let d = document.createElement('d')
+         d.classList.add("card")
+           d.innerHTML = `
+          <img src=${i.img} />
+          <p>${i.id}</p>
+          <p>${i.name}</p>
+           <p>${i.weaknesses}</p>
+           <p>${i.type}</p>
+           <p>${i.egg}</p>
+          `
+           div.appendChild(d)
+        });
+}
+
+export const typeGhost = () =>{
+       let fantasma = list.filter(gh=>gh.type.includes('Ghost'))
+           fantasma.filter(gh=>gh.type.includes('Ghost'))
+       let div = document.querySelector('#root')
+        fantasma.map(gh =>{
+         let d = document.createElement('d')
+         d.classList.add("card")
+           d.innerHTML = `
+          <img src=${gh.img} />
+          <p>${gh.id}</p>
+          <p>${gh.name}</p>
+           <p>${gh.weaknesses}</p>
+           <p>${gh.type}</p>
+           <p>${gh.egg}</p>
+          `
+           div.appendChild(d)
+        });
+}
+
+export const typeDragon = () =>{
+       let dragon = list.filter(dr=>dr.type.includes('Dragon'))
+           dragon.filter(dr=>dr.type.includes('Dragon'))
+       let div = document.querySelector('#root')
+        dragon.map(dr =>{
+         let d = document.createElement('d')
+         d.classList.add("card")
+           d.innerHTML = `
+          <img src=${dr.img} />
+          <p>${dr.id}</p>
+          <p>${dr.name}</p>
+           <p>${dr.weaknesses}</p>
+           <p>${dr.type}</p>
+           <p>${dr.egg}</p>
+          `
+           div.appendChild(d)
+        });
+}
--- src/main.js
+++ src/main.js
@@ -12,9 +12,14 @@ $(".menu-bar").on("click", function(){
 
 });
 
+
 //-----------------------------Pakiman------------------------------------------
 
-import { typeWater, typeFire } from './data.js';
+import { typeWater, typeFire, typeGrass, typePoison, typeFlying, typeBug,
+typeNormal, typeElectric, typeGround, typeFighting, typePsychic,
+typeRock, typeIce, typeGhost, typeDragon } from './data.js';
+
+
 let btn = document.querySelector('#water');
 btn.addEventListener('click', mostrar)
 function mostrar() {
@@ -27,6 +32,109 @@ btn1.addEventListener('click', mostrar1 )
 function mostrar1() {
   document.querySelector('#root').innerHTML= '';
   typeFire()
-  
+
 }
 
+let btn2 = document.querySelector('#grass');
+btn2.addEventListener('click', mostrar2 )
+function mostrar2() {
+  document.querySelector('#root').innerHTML= '';
+  typeGrass()
+
+}
+
+let btn3 = document.querySelector('#poison');
+btn3.addEventListener('click', mostrar3 )
+function mostrar3() {
+  document.querySelector('#root').innerHTML= '';
+  typePoison()
+
+}
+
+let btn4 = document.querySelector('#flying');
+btn4.addEventListener('click', mostrar4 )
+function mostrar4() {
+  document.querySelector('#root').innerHTML= '';
+  typeFlying()
+
+}
+
+let btn5 = document.querySelector('#bug');
+btn5.addEventListener('click', mostrar5 )
+function mostrar5() {
+  document.querySelector('#root').innerHTML= '';
+  typeBug()
+
+}
+
+let btn6 = document.querySelector('#normal');
+btn6.addEventListener('click', mostrar6 )
+function mostrar6() {
+  document.querySelector('#root').innerHTML= '';
+  typeNormal()
+
+}
+
+let btn7 = document.querySelector('#electric');
+btn7.addEventListener('click', mostrar7 )
+function mostrar7() {
+  document.querySelector('#root').innerHTML= '';
+  typeElectric()
+
+}
+
+let btn8 = document.querySelector('#ground');
+btn8.addEventListener('click', mostrar8 )
+function mostrar8() {
+  document.querySelector('#root').innerHTML= '';
+  typeGround()
+
+}
+
+let btn10 = document.querySelector('#fighting');
+btn10.addEventListener('click', mostrar10 )
+function mostrar10() {
+  document.querySelector('#root').innerHTML= '';
+  typeFighting()
+
+}
+
+let btn11 = document.querySelector('#psychic');
+btn11.addEventListener('click', mostrar11 )
+function mostrar11() {
+  document.querySelector('#root').innerHTML= '';
+  typePsychic()
+
+}
+
+let btn12 = document.querySelector('#rock');
+btn12.addEventListener('click', mostrar12 )
+function mostrar12() {
+  document.querySelector('#root').innerHTML= '';
+  typeRock()
+
+}
+
+let btn14 = document.querySelector('#ice');
+btn14.addEventListener('click', mostrar14 )
+function mostrar14() {
+  document.querySelector('#root').innerHTML= '';
+  typeIce()
+
+}
+
+let btn15 = document.querySelector('#ghost');
+btn15.addEventListener('click', mostrar15 )
+function mostrar15() {
+  document.querySelector('#root').innerHTML= '';
+  typeGhost()
+
+}
+
+let btn16 = document.querySelector('#dragon');
+btn16.addEventListener('click', mostrar16 )
+function mostrar16() {
+  document.querySelector('#root').innerHTML= '';
+  typeDragon()
+
+}
